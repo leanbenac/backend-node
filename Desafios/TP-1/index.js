fb2bc7ac84d10bc29fe14321d83cc613c26c552f
leanbenac <benacleandro@gmail.com>
2023-05-08 21:32:23 -0300

    Desafio 1

---
+++ Desafios/TP-1/index.js
@@ -0,0 +1,67 @@
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.nextId = 1; // Id autoincrementable
+  }
+
+  addProduct(product) {
+    // Validar que todos los campos sean obligatorios
+    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+      console.error('Todos los campos son obligatorios');
+      return;
+    }
+    
+    // Validar que el código del producto no esté repetido
+    const codeExists = this.products.some((p) => p.code === product.code);
+    if (codeExists) {
+      console.error('El código de producto ya existe');
+      return;
+    }
+
+    // Agregar el producto al arreglo
+    product.id = this.nextId++; // Asignar id autoincrementable
+    this.products.push(product);
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.error('Not found');
+    }
+  }
+}
+
+const manager = new ProductManager();
+
+// Agrego algunos productos al arreglo
+manager.addProduct({
+  title: 'Producto 1',
+  description: 'Descripción del producto 1',
+  price: 10,
+  thumbnail: '/path/to/image1.png',
+  code: 'ABC123',
+  stock: 5,
+});
+manager.addProduct({
+  title: 'Producto 2',
+  description: 'Descripción del producto 2',
+  price: 20,
+  thumbnail: '/path/to/image2.png',
+  code: 'DEF456',
+  stock: 10,
+});
+
+// Imprime el arreglo de productos
+console.log(manager.getProducts());
+
+// // Imprime el producto con id 2
+// console.log(manager.getProductById(2));
+
+// // Intenta obtener un producto que no existe
+// console.log(manager.getProductById(3)); // Output: "Not found"
