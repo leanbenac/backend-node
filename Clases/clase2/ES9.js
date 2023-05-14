074dfb0c5079cdf572cfff9b30ca3d2c8de86c01
leanbenac <benacleandro@gmail.com>
2023-05-14 13:35:23 -0300

    spreed REST

--- Clases/clase2/ES9.js
+++ Clases/clase2/ES9.js
@@ -1,16 +1,35 @@
-// Spread
+// Operado Spread
 
-const Objeto = {
+const objetoEjemplo = {
     id:1,
     nombre: 'leandro',
     apellido: 'benac',
     pais: 'Argetina'
 }
 
+const newUsuario = {
+    id:1,
+    ...objetoEjemplo
+};
+
+console.log({objetoEjemplo});
+console.log({newUsuario});
 
 // spread con numero max
-const numeros = [1 ,2 ,3 ,4 ,5 ]
+// const numeros = [1 ,2 ,3 ,4 ,5, 15, 28, 39 ,23 ]
 
-const mayor = Math.max(...numeros)
+// const mayor = Math.max(...numeros)
 
-console.log(mayor);
+// console.log(mayor);
+
+
+//////////////////////////////
+
+// operador REST | 
+
+
+const {nombre, apellido, ...resto} = objetoEjemplo
+
+console.log({nombre});
+console.log({apellido});
+console.log({resto});
