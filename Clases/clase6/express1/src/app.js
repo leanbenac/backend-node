d59c69a8f43befa4e5cdd5c17910f248048bea09
leanbenac <benacleandro@gmail.com>
2023-05-19 17:19:29 -0300

    exprees

---
+++ Clases/clase6/express1/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/bienvenida', (req, res) => {
+    const htmlRes = '<p style="color: blue; font-size: 100px;">Hola mundo desde Express!</p>';
+    res.send(htmlRes)
+});
+
+app.get('/usuario', (req, res) => {
+    const usuario = {
+        nombre: 'Leandro',
+        apellido: 'Gado',
+        edad: 33,
+        correo: 'leandrogado@example.com'
+    };
+    res.send(usuario)
+})
+
+app.listen(8080, () => {
+    console.log('servidor escuchando en el puerto 8080...');
+})
